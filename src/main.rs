6e248b5dc6940781cb5ef5e3639c870b1b081213
Simon Waidele <dont@example.com>
2024-02-15 11:32:12 +0100

    added a tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    // ...
+    println!("received package")
+}
+
+fn main() -> std::io::Result<()> {
+    // based on https://doc.rust-lang.org/std/net/struct.TcpListener.html
+    let TCP_SOCKET_ADDRESS = "0.0.0.0:8080";
+    let listener = TcpListener::bind(TCP_SOCKET_ADDRESS)?;
+    println!("Running TCP-Listener on: {TCP_SOCKET_ADDRESS}");
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
